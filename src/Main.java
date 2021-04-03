55badce58f593b96c574018a5a7c71643aa260fc
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-04-03 10:51:16 +0300

    variable corrections made

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,29 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Aytac Malkoc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int n1 = 0, n2 = 1, n3 , i, n;
+        int n1 = 0, n2 = 1, n3 , n;
 
-        System.out.print("N Sayısını Girin: ");
+        System.out.print("N number = ");
         n = scanner.nextInt();
 
         System.out.print(n1 + " " + n2);
 
-        for(i = 2; i < n; ++i)
+        for(int i = 2; i < n; ++i)
         {
             n3 = n1 + n2;
             System.out.print(" " + n3);
@@ -22,5 +33,4 @@ public class Main {
 
         System.out.println();
     }
-
 }
